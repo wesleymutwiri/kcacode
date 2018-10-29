1e030a263cf39155eb9b147614e77b37387f1671
wesleymutwiri <wesleymochiemo@gmail.com>
2018-10-29 18:25:11 +0300

    displayed first template view connection

--- assurance/urls.py
+++ assurance/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^$', views.index, name= 'index'),
+    url(r'', views.index, name= 'index'),
 ]
 
