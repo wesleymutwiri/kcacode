67922fed5e97f679de46aabeb6f4c3fae8003e48
wesleymutwiri <wesleymochiemo@gmail.com>
2018-11-11 23:40:13 +0300

    create demo model

--- assurance/models.py
+++ assurance/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+import datetime as dt 
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
+
+class Profile(models.Model):
+    username = models.CharField(max_length=60)
+    firstname = models.CharField(max_length=60)
+    lastname = models.CharField(max_length=60)
+    email = models.EmailField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    profile_avatar = ProcessedImageField(upload_to = 'avatars/', processors=[ResizeToFill(100,100)], format = 'JPEG', options ={'quality':60})
