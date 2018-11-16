3b424ebaeffc98d5824ff70f226d0607b2087cfa
wesleymutwiri <wesleymochiemo@gmail.com>
2018-11-16 22:59:53 +0300

    created views for displaying the template

--- assurance/views.py
+++ assurance/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'assurance/index.html')
+
+def about(request):
+    return render(request, 'assurance/about.html')
