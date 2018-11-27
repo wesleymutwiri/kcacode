c43e586cbc7d352e7589368c52df72de84f1622a
wesleymutwiri <wesleymochiemo@gmail.com>
2018-11-27 08:07:29 +0300

    minor changes to the structure of program

--- kcacode/urls.py
+++ kcacode/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('assurance.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
